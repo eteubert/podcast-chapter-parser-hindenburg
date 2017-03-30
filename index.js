9d03cc41f6fff17a805c7c2fdd563b49e3110309
eteubert <eric@ericteubert.de>
2017-03-30 10:07:35 +0200

    init

---
+++ index.js
@@ -0,0 +1,58 @@
+var npt = require('normalplaytime');
+
+/**
+ * Return parser instance
+ * 
+ * @param  DOMParser domparser DOMParser via dependency injection. 
+ *         Via node, xmldom can be used. In the browser, the native DOMParser is available.
+ *         
+ * @return object with `parse` method
+ */
+var parser = function(domparser) {
+    
+    var parse = function(text) {
+
+        var parser = new domparser();
+        var xml    = parser.parseFromString(text, "text/xml");
+        var chapterTags = xml.getElementsByTagName('Marker');
+
+        var chapters = [];
+
+        for (var i = 0; i < chapterTags.length; i++) {
+            var tag = chapterTags[i];
+
+            // skip markers that are not chapters
+            if (tag.getAttribute('Type').toLowerCase() !== 'chapter') {
+                continue;
+            }
+
+            var start = npt.parse(tag.getAttribute('Time'));
+            var title = tag.getAttribute('Name');
+            var href  = tag.getAttribute('URL');
+
+            if (start !== null) {
+                var chapter = {
+                    start: start,
+                    title: title.trim()
+                };
+
+                if (href) {
+                    chapter.href = href.trim();
+                }
+
+                chapters.push(chapter);
+            }
+        }
+
+        return chapters;
+
+    };
+
+    return {
+        parse: parse
+    }
+}
+
+module.exports = {
+    parser: parser
+};
---
+++ test/test.js
@@ -0,0 +1,52 @@
+var DOMParser = require('xmldom').DOMParser;
+var assert = require("assert");
+var hindenburg = require('../index.js').parser(DOMParser);
+
+const wrap = function(xml) {
+	return `<?xml version="1.0" encoding="utf-8"?>
+<Session Version="Hindenburg Journalist 1.15.1769" Samplerate="48000">
+  <AudioPool Path="Hindenburg Test Files" Location="/Users/example/Mobile Studio/Recordings Mobile Studio/Test/Hindenburg">
+	<File Id="1" Name="Track 2 1.wav" Duration="29.654" Channels="2" Leq="-36.7" Dyn="2.58"/>
+	<File Id="2" Name="Track 2 2.wav" Duration="52.182" Channels="2" Leq="-68.0"/>
+  </AudioPool>
+  <Tracks>
+	<Track Name="Track 1"/>
+	<Track Name="Track 2" Rec="1" Mute="1">
+	  <Region Ref="1" Name="Track 2 1" Length="09.406" Offset="00.145"/>
+	  <Region Ref="2" Name="Track 2 2" Start="09.406" Length="51.928" Offset="00.145"/>
+	</Track>
+	<Track Name="Track 3"/>
+	<Track Name="Track 4"/>
+  </Tracks>
+  <Clipboard>
+	<Group Caption="Group 1"/>
+	<Group Caption="Group 2"/>
+	<Group Caption="Group 3"/>
+	<Group Caption="Group 4"/>
+  </Clipboard>
+  <Markers>
+	` + xml + `
+  </Markers>
+</Session>`;
+}
+
+describe('hindenburg', function() {
+	describe('#parse()', function() {
+
+	    it('should return empty list for no chapters', function() {
+	    	assert.deepEqual([], hindenburg.parse(wrap('<Marker Id="1" Name="Marker 1" Time="21.200" Type="NotAChapter"/>')));
+	    });
+
+		it('should return json with start, title and href', function() {
+			assert.deepEqual([{ start: 1200, title: "Intro", href: "http://example.com" }], hindenburg.parse(wrap('<Marker Id="1" Name="Intro" Time="1.200" Type="Chapter" URL="http://example.com"/>')));
+		});
+
+		it('should read multiple chapters', function() {
+			assert.deepEqual([
+				{ start: 1200, title: "Intro" },
+				{ start: 2400, title: "Second" }
+			], hindenburg.parse(wrap('<Marker Id="1" Name="Intro" Time="1.200" Type="Chapter"/><Marker Id="2" Name="Second" Time="2.400" Type="Chapter"/>')));
+		});
+
+	});
+});
